e01d7b485eb2a50141f891bb463396b93d5d2148
joselabourdette <josecerebro@gmail.com>
2025-11-14 10:28:02 -0300

    arregle texto

--- frontend/src/components/PublicacionesDestacadas.jsx
+++ frontend/src/components/PublicacionesDestacadas.jsx
@@ -86,7 +86,7 @@ export default function PublicacionesDestacadas() {
   
   return (
     <div className="contenido-p">
-      <h3 className="Destacados">publicaiones Destacadas</h3>;
+      <h3 className="Destacados">Publicaciones Destacadas</h3>;
         <div className="publicaciones-grid">
           {publicaciones.map((pub) => {
             const imgSrc =
