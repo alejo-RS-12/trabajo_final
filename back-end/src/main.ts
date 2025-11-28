634586188118a1d73df7925a0f940a7f2c52fb1a
joselabourdette <josecerebro@gmail.com>
2025-11-28 01:02:50 -0300

    modif main.ts ecpress

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -3,17 +3,21 @@ import { AppModule } from './app.module';
 import * as express from 'express';
 import { join } from 'path';
 import { METHODS } from 'http';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors({origin: ["https://ropo-7e929.web.app",
       "https://ropo-7e929.firebaseapp.com", "http://localhost:5173"], 
-    METHODS: "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+    methods: "GET, POST, PUT, DELETE, PATCH, OPTIONS",
     credentials: true,});
   // Carpeta uploads accesible públicamente
   app.use('/uploads', express.static(join(__dirname, '..', 'uploads')));
 
-  
+  app.useStaticAssets(join(__dirname, '..', 'uploads'), {
+    prefix: '/uploads/',
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
